5c628f8a80a8095cb22c2b0953af61afb28b08b0
huijie <huijie@hudongpai.com>
2018-07-13 10:19:12 +0800

    学习札记

---
+++ learn/example/js-pro/promise.php
@@ -0,0 +1,13 @@
+<?php 
+	function example1(){
+		$arr = '[
+				"success": true, 
+				"data" :{
+					"name": "litiantian", 
+					"chooce": "scuec"
+				}
+			]';
+		echo $arr;
+	};
+	example1();
+?>
