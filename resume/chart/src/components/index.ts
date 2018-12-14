b4d336f13a8d2b13242070e4fb85d63a40992b67
huijie <huijie@hudongpai.com>
2018-12-14 11:46:44 +0800

    迁移代码至resume-chart

--- resume/chart/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue';
-import gComponents from './packages/index'; // 全局组件
-
-Vue.use(gComponents);
--- resume/chart/src/views/About.vue
+++
@@ -1,7 +0,0 @@
-<template>
-	<div class="about">
-		<h1>This is an about page</h1>
-
-		<g-card></g-card>
-	</div>
-</template>
